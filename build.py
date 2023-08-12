464674021f9edc93481f93f26e5c32164d667e52
mrkubax10 <mrkubax10@onet.pl>
2023-08-12 08:53:16 +0200

    Improvements to build system
    Changes in appropriate files are now detected so project is not fully rebuilt every time

--- build.py
+++ build.py
@@ -1,62 +1,208 @@
 #!/usr/bin/python3
 
-CFLAGS='-m32 -c -Wno-int-to-pointer-cast -nostdlib -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -nodefaultlibs -I . '
-ASFLAGS='-f elf '
-
-csources = []
-ssources = []
+# QuantumOS Copyright (c) 2022-2023
+# - SolindekDev <solindeklive.biznes@gmail.com>
+# - mrkubax10 <mrkubax10@onet.pl>
 
 import fnmatch
 import os
+import sys
+import json
+import subprocess
 
-for root, dirnames, filenames in os.walk('.'):
-    for filename in fnmatch.filter(filenames, '*.c'):
-        if "apps" not in os.path.join(root, filename) and "clib" not in os.path.join(root, filename):
-            csources.append(os.path.join(root, filename))
+class Arguments:
+	def __init__(self,args):
+		self.cc=None
+		self.asm=None
+		self.grub_rescue=None
+		self.run=False
+		self.should_rebuild=False
+		index=1
+		while index<len(args):
+			arg = args[index]
+			if arg=="--cc":
+				index+=1
+				if index>=len(args):
+					print("--cc requires argument")
+					exit(1)
+				self.cc=args[index]
+				self.should_rebuild=True
+			elif arg=="--asm":
+				index+=1
+				if index>=len(args):
+					print("--asm requires argument")
+					exit(1)
+				self.asm=args[index]
+				self.should_rebuild=True
+			elif arg=="--grub-rescue":
+				index+=1
+				if index>=len(args):
+					print("--grub-rescue requires argument")
+					exit(1)
+				self.grub_rescue=args[index]
+			elif arg=="run":
+				self.run=True
+			elif arg=="help" or arg=="--help" or arg=="-h":
+				self.print_help(args[0])
+				exit(0)
+			elif arg=="--rebuild":
+				self.should_rebuild=True
+			else:
+				print("Invalid argument: "+arg)
+				exit(1)
+			index+=1
 
-for root, dirnames, filenames in os.walk('.'):
-    for filename in fnmatch.filter(filenames, '*.asm'):
-        if "apps" not in os.path.join(root, filename) and "clib" not in os.path.join(root, filename):
-            ssources.append(os.path.join(root, filename))
+	def print_help(self,executable):
+		print("QuantumOS build system")
+		print("Usage: "+executable+" [subcommand] [arguments...]")
+		print("Subcommands:")
+		print("  run                            Start QEMU after build finishes")
+		print("  help                           Show this message")
+		print("Arguments:")
+		print("  --cc <compiler>                Specify C compiler")
+		print("  --asm <assembler>              Specify assembler")
+		print("  --grub-rescue <grub rescue>    Specify GRUB Rescue for creating ISO image")
+		print("  --rebuild                      Full rebuild")
 
-for i in range(len(csources)):
-    c = csources[i]
 
-    os.system("mkdir -p build/" + c)
+class BuildConfig:
+	def __init__(self,args):
+		self.csources={}
+		self.ssources={}
+		self.should_rebuild=args.should_rebuild
+		self.collect_sources()
+		if args.should_rebuild or not self.try_load_config():
+			self.generate_config(args)
+		self.write_config()
 
-    print("[" + str(i + 1) + "/" + str(len(csources)) + "] " + "Compiling: [" + c + "]")
+	def collect_sources(self):
+		for root, dirnames, filenames in os.walk('.'):
+			for filename in fnmatch.filter(filenames, '*.c'):
+				if "apps" not in os.path.join(root, filename) and "clib" not in os.path.join(root, filename):
+					p = os.path.join(root, filename)
+					self.csources[p]=os.path.getmtime(p)
 
-    os.system("i686-elf-gcc " + CFLAGS + c + " -o build/" + c + ".o")
+		for root, dirnames, filenames in os.walk('.'):
+			for filename in fnmatch.filter(filenames, '*.asm'):
+				if "apps" not in os.path.join(root, filename) and "clib" not in os.path.join(root, filename):
+					p = os.path.join(root, filename)
+					self.ssources[p]=os.path.getmtime(p)
 
-for i in range(len(ssources)):
-    s = ssources[i]
+	def generate_config(self,args):
+		print("** Generating build config...")
+		self.determine_cc(args)
+		print("C compiler on this system is " + self.cc)
+		self.determine_asm(args)
+		print("Assembler on this system is " + self.asm)
+		self.determine_grub_rescue(args)
+		print("GRUB Rescue on this system is "+self.grub_rescue)
 
-    os.system("mkdir -p build/" + s)
+	def determine_cc(self,args):
+		if args.cc:
+			self.cc=args.cc
+		else:
+			self.cc=os.getenv("CC","cc")
 
-    print("[" + str(i + 1) + "/" + str(len(ssources)) + "] "+ "Compiling: [" + s + "]")
+	def determine_asm(self,args):
+		if args.asm:
+			self.asm=args.asm
+		else:
+			self.asm="nasm"
+	
+	def determine_grub_rescue(self,args):
+		if args.grub_rescue:
+			self.grub_rescue=args.grub_rescue
+		else:
+			self.grub_rescue="grub-mkcrescue"
 
-    os.system("nasm " + ASFLAGS + s + " -o build/" + s + ".o")
+	def try_load_config(self):
+		file=None
+		try:
+			file=open("build_config.json","r")
+		except FileNotFoundError:
+			return False
+		json_config=json.load(file)
+		file.close()
+		REQUIRED_VALUES=["cc","asm","grub_rescue","cflags","asmflags","csources","ssources"]
+		for val in REQUIRED_VALUES:
+			if not val in json_config:
+				return False
+		self.cc=json_config["cc"]
+		self.asm=json_config["asm"]
+		self.grub_rescue=json_config["grub_rescue"]
+		self.cflags=json_config["cflags"]
+		self.asmflags=json_config["asmflags"]
+		self.old_csources=json_config["csources"]
+		self.old_ssources=json_config["ssources"]
+		return True
 
-total = ""
+	def write_config(self):
+		self.cflags=["-m32", "-Wno-int-to-pointer-cast", "-nostdlib", "-nostdinc", "-fno-builtin", "-fno-stack-protector", "-nostartfiles", "-nodefaultlibs", "-I."]
+		self.asmflags=["-f", "elf"]
+		json_config={
+			"cc": self.cc,
+			"asm": self.asm,
+			"grub_rescue": self.grub_rescue,
+			"cflags": self.cflags,
+			"asmflags": self.asmflags,
+			"csources": self.csources,
+			"ssources": self.ssources
+		}
+		file=open("build_config.json","w")
+		json.dump(json_config, file)
+		file.close()
 
-os.system("mkdir -p build/iso/boot/grub/")
 
-for c in csources:
-    total += "build/"
-    total += c
-    total += ".o "
+def check_result_code(result):
+	if result.returncode != 0:
+		print("** Previous subcommand failed. Stopping.")
+		exit(1)
 
-for s in ssources:
-    total += "build/"
-    total += s
-    total += ".o "
 
-print("\nLinking kernel...\n")
+args=Arguments(sys.argv)
+config=BuildConfig(args)
 
-os.system("i686-elf-ld -m elf_i386 -Tconfig/linker.ld -o build/iso/boot/kernel.elf " + total)
-os.system("cp -f config/grub.cfg build/iso/boot/grub/")
-os.system("grub-mkrescue -o build/quantumos.iso build/iso")
+print("** Building...")
+i=1
+for c in config.csources:
+	if not config.should_rebuild and config.csources[c]==config.old_csources[c]:
+		continue
+	folder_path = "build/"+os.path.dirname(c)
+	if not os.path.exists(folder_path):
+		os.makedirs(folder_path)
+	print("[" + str(i + 1) + "/" + str(len(config.csources)) + "] " + "Compiling: [" + c + "]")
+	result = subprocess.run([config.cc, "-c", c, "-o", "build/" + c + ".o"] + config.cflags)
+	check_result_code(result)
+	i+=1
 
-print("Done!\nRunning kernel...")
+i=1
+for s in config.ssources:
+	if not config.should_rebuild and config.old_ssources[s]==config.ssources[s]:
+		continue
+	folder_path = "build/"+os.path.dirname(s)
+	if not os.path.exists(folder_path):
+		os.makedirs(folder_path)
+	print("[" + str(i + 1) + "/" + str(len(config.ssources)) + "] "+ "Assembling: [" + s + "]")
+	result = subprocess.run([config.asm, s, "-o", "build/" + s + ".o"] + config.asmflags)
+	check_result_code(result)
+	i+=1
 
-os.system("qemu-system-x86_64 -cdrom ./build/quantumos.iso -m 3G -vga vmware -serial stdio -device ac97")
+grub_folder="build/iso/boot/grub"
+if not os.path.exists(grub_folder):
+	os.makedirs(grub_folder)
+
+total = []
+for c in dict(config.csources,**config.ssources):
+	total.append("build/" + c +".o")
+
+print("** Linking kernel...")
+result = subprocess.run(["ld", "-m", "elf_i386", "-Tconfig/linker.ld", "-o", "build/iso/boot/kernel.elf"] + total)
+check_result_code(result)
+result = subprocess.run(["cp", "-f", "config/grub.cfg", "build/iso/boot/grub"])
+check_result_code(result)
+result = subprocess.run([config.grub_rescue, "-o", "build/quantumos.iso", "build/iso"])
+check_result_code(result)
+
+if args.run:
+	print("** Running kernel...")
+	subprocess.run(["qemu-system-x86_64", "-cdrom", "./build/quantumos.iso", "-m", "3G", "-vga", "vmware", "-serial", "stdio"])
