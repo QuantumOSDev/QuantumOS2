ff998005227343b954328fc635b8cf7240eb2eff
mrkubax10 <mrkubax10@onet.pl>
2023-08-20 21:31:34 +0200

    Create exFAT system image instead of iso

--- build.py
+++ build.py
@@ -15,7 +15,7 @@ class Arguments:
 		self.cc=None
 		self.asm=None
 		self.ld=None
-		self.grub_rescue=None
+		self.grub_install=None
 		self.run=False
 		self.should_rebuild=False
 		index=1
@@ -41,12 +41,12 @@ class Arguments:
 					print("--ld requires argument")
 					exit(1)
 				self.ld=args[index]
-			elif arg=="--grub-rescue":
+			elif arg=="--grub-install":
 				index+=1
 				if index>=len(args):
-					print("--grub-rescue requires argument")
+					print("--grub-install requires argument")
 					exit(1)
-				self.grub_rescue=args[index]
+				self.grub_install=args[index]
 			elif arg=="run":
 				self.run=True
 			elif arg=="help" or arg=="--help" or arg=="-h":
@@ -69,7 +69,7 @@ class Arguments:
 		print("  --cc <compiler>                Specify C compiler")
 		print("  --asm <assembler>              Specify assembler")
 		print("  --ld <linker>                  Specify linker")
-		print("  --grub-rescue <grub rescue>    Specify GRUB Rescue for creating ISO image")
+		print("  --grub-install <grub-install>  Specify grub-install for installing bootloader on disk image")
 		print("  --rebuild                      Full rebuild")
 
 
@@ -103,8 +103,8 @@ class BuildConfig:
 		print("Assembler on this system is " + self.asm)
 		self.determine_ld(args)
 		print("Linker on this system is "+self.ld)
-		self.determine_grub_rescue(args)
-		print("GRUB Rescue on this system is "+self.grub_rescue)
+		self.determine_grub_install(args)
+		print("grub-install on this system is "+self.grub_install)
 
 	def determine_cc(self,args):
 		if args.cc:
@@ -124,11 +124,11 @@ class BuildConfig:
 		else:
 			self.ld="ld"
 
-	def determine_grub_rescue(self,args):
-		if args.grub_rescue:
-			self.grub_rescue=args.grub_rescue
+	def determine_grub_install(self,args):
+		if args.grub_install:
+			self.grub_install=args.grub_install
 		else:
-			self.grub_rescue="grub-mkrescue"
+			self.grub_install="grub-install"
 
 	def try_load_config(self):
 		file=None
@@ -138,14 +138,14 @@ class BuildConfig:
 			return False
 		json_config=json.load(file)
 		file.close()
-		REQUIRED_VALUES=["cc","asm","ld","grub_rescue","cflags","asmflags","csources","ssources"]
+		REQUIRED_VALUES=["cc","asm","ld","grub_install","cflags","asmflags","csources","ssources"]
 		for val in REQUIRED_VALUES:
 			if not val in json_config:
 				return False
 		self.cc=json_config["cc"]
 		self.asm=json_config["asm"]
 		self.ld=json_config["ld"]
-		self.grub_rescue=json_config["grub_rescue"]
+		self.grub_install=json_config["grub_install"]
 		self.cflags=json_config["cflags"]
 		self.asmflags=json_config["asmflags"]
 		self.old_csources=json_config["csources"]
@@ -159,7 +159,7 @@ class BuildConfig:
 			"cc": self.cc,
 			"asm": self.asm,
 			"ld": self.ld,
-			"grub_rescue": self.grub_rescue,
+			"grub_install": self.grub_install,
 			"cflags": self.cflags,
 			"asmflags": self.asmflags,
 			"csources": self.csources,
@@ -175,6 +175,21 @@ def check_result_code(result):
 		print("** Previous subcommand failed. Stopping.")
 		exit(1)
 
+def mount_system_image(format, mountpoint_folder):
+	print("Following operations will mount (and format disk image), these operations will require root access.")
+	print("Please check build.py source code to verify what code will be executed.")
+	if format:
+		check_result_code(subprocess.run(["sudo", "parted", "-s", "build/disk.img", "mklabel", "msdos", "mkpart", "primary", "fat32", "1MiB", "100%", "-a", "minimal", "set", "1", "boot", "on"]))
+	check_result_code(subprocess.run(["sudo", "kpartx", "-a", "build/disk.img"]))
+	if format:
+		check_result_code(subprocess.run(["sudo", "mkfs.exfat", "/dev/mapper/loop0p1"]))
+	check_result_code(subprocess.run(["sudo", "mount", "/dev/mapper/loop0p1", mountpoint_folder, "-o", "umask=000"]))
+	if format:
+		check_result_code(subprocess.run(["sudo", config.grub_install, "--no-floppy", "--grub-mkdevicemap=config/grub_devicemap.map", "--modules=biosdisk part_msdos  exfat configfile normal multiboot", "--root-directory="+mountpoint_folder, "--boot-directory="+mountpoint_folder+"/boot", "/dev/loop0", "--target=i386-pc"]))
+
+def unmount_system_image(mountpoint_folder):
+	check_result_code(subprocess.run(["sudo", "umount", mountpoint_folder]))
+	check_result_code(subprocess.run(["sudo", "kpartx", "-d", "build/disk.img"]))
 
 args=Arguments(sys.argv)
 config=BuildConfig(args)
@@ -189,7 +204,7 @@ for c in config.csources:
 	folder_path = "build/"+os.path.dirname(c)
 	if not os.path.exists(folder_path):
 		os.makedirs(folder_path)
-	print("[" + str(i + 1) + "/" + str(len(config.csources)) + "] " + "Compiling: [" + c + "]")
+	print("[" + str(i) + "/" + str(len(config.csources)) + "] " + "Compiling: [" + c + "]")
 	result = subprocess.run([config.cc, "-c", c, "-o", "build/" + c + ".o"] + config.cflags)
 	check_result_code(result)
 	i+=1
@@ -201,27 +216,33 @@ for s in config.ssources:
 	folder_path = "build/"+os.path.dirname(s)
 	if not os.path.exists(folder_path):
 		os.makedirs(folder_path)
-	print("[" + str(i + 1) + "/" + str(len(config.ssources)) + "] "+ "Assembling: [" + s + "]")
+	print("[" + str(i) + "/" + str(len(config.ssources)) + "] "+ "Assembling: [" + s + "]")
 	result = subprocess.run([config.asm, s, "-o", "build/" + s + ".o"] + config.asmflags)
 	check_result_code(result)
 	i+=1
 
-grub_folder="build/iso/boot/grub"
-if not os.path.exists(grub_folder):
-	os.makedirs(grub_folder)
+mountpoint_folder="build/mountpoint"
+if not os.path.exists(mountpoint_folder):
+	os.makedirs(mountpoint_folder)
 
 total = []
 for c in dict(config.csources,**config.ssources):
 	total.append("build/" + c +".o")
-
 print("** Linking kernel...")
-result = subprocess.run([config.ld, "-m", "elf_i386", "-Tconfig/linker.ld", "-o", "build/iso/boot/kernel.elf"] + total)
-check_result_code(result)
-result = subprocess.run(["cp", "-f", "config/grub.cfg", "build/iso/boot/grub"])
-check_result_code(result)
-result = subprocess.run([config.grub_rescue, "-o", "build/quantumos.iso", "build/iso"])
-check_result_code(result)
+check_result_code(subprocess.run([config.ld, "-m", "elf_i386", "-Tconfig/linker.ld", "-o", "build/kernel.elf"] + total))
+
+if os.path.exists("build/disk.img"):
+	print("** Mounting system image...")
+	mount_system_image(False, mountpoint_folder)
+else:
+	print("** Creating new system image...")
+	check_result_code(subprocess.run(["dd", "if=/dev/zero", "of=build/disk.img", "bs=1048576", "count=128"]))
+	mount_system_image(True, mountpoint_folder)
+check_result_code(subprocess.run(["cp", "-f", "config/grub.cfg", mountpoint_folder+"/boot/grub2"]))
+check_result_code(subprocess.run(["cp", "-f", "build/kernel.elf", mountpoint_folder+"/boot/kernel.elf"]))
+print("** Unmounting system image...")
+unmount_system_image(mountpoint_folder)
 
 if args.run:
 	print("** Running kernel...")
-	subprocess.run(["qemu-system-x86_64", "-cdrom", "./build/quantumos.iso", "-m", "3G", "-vga", "vmware", "-serial", "stdio"])
+	subprocess.run(["qemu-system-x86_64", "-drive", "if=ide,index=0,media=disk,format=raw,file=build/disk.img", "-m", "3G", "-vga", "vmware", "-serial", "stdio"])
